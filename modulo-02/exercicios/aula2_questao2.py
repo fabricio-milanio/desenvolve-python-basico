44046dda861eb0e5d3babe77893463c0d264b71f
Fabricio Milanio <milanio.fabricio@gmail.com>
2025-07-10 16:16:58 -0300

    mod_2_exer_2

---
+++ modulo-02/exercicios/aula2_questao2.py
@@ -0,0 +1,24 @@
+"""
+2 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+
+Some os valores da segunda e terceira variável e armazene em outra variável.
+
+Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+"""
+
+resultado_texto = "o resultado é"
+num1 = 10
+num2 = 3.5
+soma = num1 + num2
+
+print(resultado_texto)
+print(type(resultado_texto))
+print(num1)
+print(type(num1))
+print(num2)
+print(type(num2))
+print(soma)
+print(type(soma))
