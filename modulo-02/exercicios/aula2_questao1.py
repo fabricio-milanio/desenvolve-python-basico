11598a6a06e3424ae5adec1dcda219b80fba38c6
Fabricio Milanio <milanio.fabricio@gmail.com>
2025-07-10 16:09:51 -0300

    mod_2_exer_1

---
+++ modulo-02/exercicios/aula2_questao1.py
@@ -0,0 +1,23 @@
+"""
+1 - Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+Armazene o valor 5 em uma variável e o valor 2 em outra variável
+
+Imprima o tipo de dados dessas duas variáveis
+
+Divida a primeira variável (com o valor 5) pela segunda variável (com o valor 2) e armazene o resultado em uma terceira variável
+
+Imprima a terceira variável e também o seu tipo
+
+"""
+
+num1 = 5
+num2 = 2
+
+print("O número", num1, "é do tipo", type(num1))
+print("O número", num2, "é do tipo", type(num2))
+
+resultado = num1 / num2
+
+print(resultado)
+print("O número", resultado, "é do tipo", type(resultado))
