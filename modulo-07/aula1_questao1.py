bc334011bcc90b74637967d2cdbe0db5d3382006
Fabricio Milanio <milanio.fabricio@gmail.com>
2025-08-07 18:40:55 -0300

    mod_7_aula_1_exer_1

---
+++ modulo-07/aula1_questao1.py
@@ -0,0 +1,21 @@
+'''
+Escreva um programa que solicita o nome do usuário e o imprime em forma de escada, como indicado no exemplo a seguir.
+
+Digite seu nome: Fulano
+
+F
+
+Fu
+
+Ful
+
+Fula
+
+Fulan
+
+Fulano
+'''
+
+nome = input("Digite seu nome: ")
+for i in range(1, len(nome) + 1):
+    print(nome[:i])
